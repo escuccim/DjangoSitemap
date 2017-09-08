5660c7a1acf123205acce7fafe6b1a70de4f6f62
Eric Scuccimarra <skooch@gmail.com>
2017-09-08 12:14:37 +0200

    Added hreflang tags, function to display blog posts

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ app_name = "sitemap"
 urlpatterns = [
     url(r'^$', views.Index, name='index'),
     url(r'^pages$', views.Pages, name='pages'),
+    url(r'^blog$', views.Blog, name='blogs'),
 ]
--- views.py
+++ views.py
@@ -5,16 +5,26 @@ from django.shortcuts import render
 from .models import Sitemap, Page
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from django.utils import translation
-
+from blog import models as blog_models
+from django.conf import settings
 
 # Create your views here.
 def Index(request):
     sitemaps = Sitemap.objects.all()
     language = translation.get_language()
-    return render(request, 'sitemap/sitemapindex.html', { 'sitemaps': sitemaps, 'language' : language})
+    languages = settings.LANGUAGES
+    return render(request, 'sitemap/sitemapindex.html', { 'sitemaps': sitemaps, 'language' : language, 'languages' : languages})
 
 
 def Pages(request):
     pages = Page.objects.all()
     language = translation.get_language()
-    return render(request, 'sitemap/pages.html', {'pages': pages, 'language': language})
+    languages = settings.LANGUAGES
+    return render(request, 'sitemap/pages.html', {'pages': pages, 'language': language, 'languages' : languages})
+
+
+def Blog(request):
+    blogs = blog_models.Blog.objects.all()
+    language = translation.get_language()
+    languages = settings.LANGUAGES
+    return render(request, 'sitemap/blogs.html', { 'blogs' : blogs, 'language': language, 'languages' : languages })
