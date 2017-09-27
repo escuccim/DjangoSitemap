3b11396e2d3cd25b9b78a835c4cbc758e7ced36c
Eric Scuccimarra <skooch@gmail.com>
2017-09-27 12:15:50 +0200

    Admin for images

--- admin.py
+++ admin.py
@@ -5,13 +5,14 @@ from django.contrib import admin
 from .models import Page,Sitemap,SitemapImage
 # Register your models here.
 
+
 class ImageInline(admin.TabularInline):
     model = SitemapImage
     extra = 3
 
+
 class PageAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
 
-
 admin.site.register(Page, PageAdmin)
-admin.site.register(Sitemap)
+admin.site.register(Sitemap,SitemapImage)
